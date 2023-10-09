9956570a1a13129821657c6bb30b74271c9e6d90
wanl1n <kate_young@dlsu.edu.ph>
2023-10-09 17:41:20 +0800

    Objective Tracker + Pause Menu

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject PauseMenu;
+    public GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
@@ -25,5 +29,30 @@ public class ButtonManager : MonoBehaviour
     public void Leave()
     {
         Application.Quit();
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        Player.GetComponent<BasicBehaviour>().enabled = false;
+        Player.GetComponent<MoveBehaviour>().enabled = false;
+        Player.GetComponent<AimBehaviourBasic>().enabled = false;
+        Player.GetComponentInChildren<ThirdPersonOrbitCamBasic>().enabled = false;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        Player.GetComponent<BasicBehaviour>().enabled = true;
+        Player.GetComponent<MoveBehaviour>().enabled = true;
+        Player.GetComponent<AimBehaviourBasic>().enabled = true;
+        Player.GetComponentInChildren<ThirdPersonOrbitCamBasic>().enabled = true;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
